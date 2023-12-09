4a9a41ea7cb0e198f5ce88123f23fb138da444d5
singhet <singhet@SCSWL4072>
2023-12-09 09:42:27 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Title = "Tytuł pierwszej storny";
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("Hello, World!");
+Console.Write("nie ma nowej linijki 1 ");
+Console.Write("jeszcze raz nie ma linijki 2");
+Console.ResetColor();
+Console.ReadKey();
+Console.Clear();
+
+Console.Title = "Tytuł drugiej storny";
+Console.WriteLine("Hello, again!");
+Console.Write("naciśnij coś aby przejść do kolejnej strony");
+Console.ReadKey();
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("Teraz nowa linijka 3");
+
+Console.Title = "Tytuł trzeciej storny";
+Console.Write("naciśnij coś aby przejść do kolejnej strony");
+Console.ReadKey();
+Console.ResetColor();
+Console.Clear();
+
+Console.Title = "The end";
+Console.WriteLine("kolejna nowa linijka 4");
+Console.Write("Wciskaj cokolwiek to zamknie się program");
+Console.ReadKey();
